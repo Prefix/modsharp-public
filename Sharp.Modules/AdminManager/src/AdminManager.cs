cdf87d78036eeebf2def806c1d0fa72cbb4a5817
Nukoooo <89713806+Nukoooo@users.noreply.github.com>
2026-01-05 19:02:30 +0800

    copyright, en doc for ICommandManager

--- Sharp.Modules/AdminManager/src/AdminManager.cs
+++ Sharp.Modules/AdminManager/src/AdminManager.cs
@@ -1,3 +1,22 @@
+/*
+ * ModSharp
+ * Copyright (C) 2023-2025 Kxnrl. All Rights Reserved.
+ *
+ * This file is part of ModSharp.
+ * ModSharp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ModSharp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ModSharp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // ReSharper disable UnusedParameter.Local
 
 using System.Text.Json;
@@ -190,7 +209,6 @@ internal class AdminManager : IAdminManager, IModSharpModule
             }
         }
 
-        // Mount permission collections for this module
         // Mount permission collections for this module
         if (!_permissionCollections.TryGetValue(moduleIdentity, out var modulePermissionCollection))
         {
--- Sharp.Modules/CommandManager/Shared/ICommandManager.cs
+++ Sharp.Modules/CommandManager/Shared/ICommandManager.cs
@@ -1,3 +1,22 @@
+/*
+ * ModSharp
+ * Copyright (C) 2023-2025 Kxnrl. All Rights Reserved.
+ *
+ * This file is part of ModSharp.
+ * ModSharp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ModSharp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ModSharp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using Sharp.Shared.Objects;
 using Sharp.Shared.Types;
 using DelegateClientCommand = Sharp.Shared.Managers.IClientManager.DelegateClientCommand;
@@ -9,9 +28,9 @@ public interface ICommandManager
     const string Identity = nameof(ICommandManager);
 
     /// <summary>
-    /// Add Command registry. <br/>
+    ///     Gets the command registry for the specified module identity.
     /// </summary>
-    /// <param name="moduleIdentity"></param>
+    /// <param name="moduleIdentity">The unique identity of the module.</param>
     public ICommandRegistry GetRegistry(string moduleIdentity);
 
 }
@@ -19,62 +38,64 @@ public interface ICommandManager
 public interface ICommandRegistry
 {
     /// <summary>
-    ///     注册用户指令。<br />
-    ///     该指令可被如下途径调用：<br />
-    ///     1. 聊天栏中输入 .{该指令内容}，例：.ztele。<br />
-    ///     (注：你可以将.换成!或/，它们都是一样的。)<br />
-    ///     2. 客户端控制台，你必须添加`ms_`前缀，例：ms_ztele。<br />
-    ///     此函数注册的指令无法被服务端控制台调用。
+    ///     Registers a client command.<br />
+    ///     The command can be invoked in the following ways:<br />
+    ///     1. Chat: Type .{<see cref="command" />} (e.g., <b>.ztele</b>).<br />
+    ///     (Note: You can replace "." with "!" or "/"; they function identically.)<br />
+    ///     2. Client Console: Requires the `ms_` prefix (e.g., <b>ms_ztele</b>).<br />
+    ///     Commands registered via this method CANNOT be used in the server console.
     /// </summary>
-    /// <param name="command">指令，你可以不添加ms_前缀，注册的时候会给你自动加上。你也可以加上，这个不影响。但是如果你是ms_ms_这种，那我们爱莫能助。</param>
-    /// <param name="call"></param>
+    /// <param name="command">
+    ///     The command name. The prefix 'ms_' is automatically added during registration.
+    ///     Do not include 'ms_' manually, or the command will result in a double prefix (e.g., "ms_ms_command").
+    /// </param>
+    /// <param name="call">The callback function to execute.</param>
     void RegisterClientCommand(string command, Action<IGameClient, StringCommand> call);
 
     /// <summary>
-    ///     创建一个控制台指令。<br />
-    ///     该指令只会在服务端控制台生效。
+    ///     Registers a server command.<br />
+    ///     Commands registered with this method only work in the server console.
     /// </summary>
-    /// <param name="description"></param>
-    /// <param name="addPrefix">是否要添加ms_前缀？默认添加。</param>
-    /// <param name="command"></param>
-    /// <param name="call"></param>
+    /// <param name="command">The command name.</param>
+    /// <param name="call">The callback function to execute.</param>
+    /// <param name="description">The command description.</param>
+    /// <param name="addPrefix">Whether to add the 'ms_' prefix to the command. Defaults to true.</param>
     void RegisterServerCommand(string command, Action<StringCommand> call, string description = "", bool addPrefix = true);
 
     /// <summary>
-    ///     创建一个控制台指令。<br />
-    ///     该指令只会在服务端控制台生效。
+    ///     Registers a server command.<br />
+    ///     Commands registered with this method only work in the server console.
     /// </summary>
-    /// <param name="description"></param>
-    /// <param name="addPrefix">是否要添加ms_前缀？默认添加。</param>
-    /// <param name="command"></param>
-    /// <param name="call"></param>
+    /// <param name="command">The command name.</param>
+    /// <param name="call">The callback function to execute.</param>
+    /// <param name="description">The command description.</param>
+    /// <param name="addPrefix">Whether to add the 'ms_' prefix to the command. Defaults to true.</param>
     void RegisterServerCommand(string command, Action call, string description = "", bool addPrefix = true);
 
     /// <summary>
-    /// 注册一个「通用」指令：客户端聊天栏，客户端控制台，服务端控制台均可使用。<br />
-    /// 一定会添加ms_标签，请注意
+    ///     Registers a "generic" command usable in in-game chat, client console, and server console.
+    ///     <br />
+    ///     Note: This will strictly enforce the 'ms_' prefix.
     /// </summary>
-    /// <param name="command"></param>
-    /// <param name="call"></param>
-    /// <param name="description"></param>
+    /// <param name="command">The command name.</param>
+    /// <param name="call">The callback function to execute.</param>
+    /// <param name="description">The command description.</param>
     void RegisterGenericCommand(string command, Action<IGameClient?, StringCommand> call, string description = "");
 
     /// <summary>
-    ///     创建一个控制台指令。 <br />
-    ///     该指令只会在客户端控制台和服务端控制台生效。<br />
+    ///     Registers a console command. <br />
+    ///     Commands registered with this method work in both the client console and server console (but not chat).
     /// </summary>
-    /// <param name="command"></param>
-    /// <param name="callback"></param>
-    /// <param name="addPrefix">是否添加ms_前缀？默认添加。</param>
+    /// <param name="command">The command name.</param>
+    /// <param name="callback">The callback function to execute.</param>
+    /// <param name="addPrefix">Whether to add the 'ms_' prefix to the command. Defaults to true.</param>
     void RegisterConsoleCommand(string command, Action<IGameClient?, StringCommand> callback, bool addPrefix = true);
 
     /// <summary>
-    ///     监听指令。<br />
-    ///     一般来说，该函数只用于监听客户端控制台内输入的指令，如player_ping。<br />
-    ///     可自行参阅函数调用。
+    ///     Adds a listener for a specific command.<br />
+    ///     This is primarily used to listen for commands entered in the client console (e.g., player_ping).<br />
     /// </summary>
-    /// <param name="commandName"></param>
-    /// <param name="callback"></param>
+    /// <param name="commandName">The name of the command to listen for.</param>
+    /// <param name="callback">The callback delegate.</param>
     void AddCommandListener(string commandName, DelegateClientCommand callback);
-
 }
