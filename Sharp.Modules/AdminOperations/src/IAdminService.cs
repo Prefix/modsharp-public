ad1358af67b9d47f6158bef2d4255e79fe1d0652
laper32 <laper32@outlook.com>
2026-01-10 13:34:49 +0800

    feat: init AdminCommands

---
+++ Sharp.Modules/AdminOperations/src/IAdminService.cs
@@ -0,0 +1,35 @@
+namespace Sharp.Modules.AdminOperations;
+
+// 不对外，这个只是为了方便
+internal interface IAdminService
+{
+    void OnInit()
+    {
+
+    }
+
+    void OnPostInit()
+    {
+
+    }
+
+    void OnLibraryConnected(string name)
+    {
+
+    }
+
+    void OnLibraryDisconnect(string name)
+    {
+
+    }
+
+    void OnAllModulesLoaded()
+    {
+
+    }
+
+    void OnShutdown()
+    {
+
+    }
+}
